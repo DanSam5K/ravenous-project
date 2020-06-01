2612a5de4685cf531212070863b7201077428d1a
DanSam%k <dansam5000@gmail.com>
2020-06-01 12:54:26 +0100

    set yelp on search and handlit search

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -19,6 +19,7 @@ class SearchBar extends React.Component {
         }
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
    }
 
     getSortByClass (sortByOption) {
@@ -41,6 +42,11 @@ class SearchBar extends React.Component {
         this.setState({ location: event.target.value });
     }
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
    renderSortByOptions() {
        return Object.keys(this.sortByOptions).map(sortByOption => {
         let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -56,11 +62,11 @@ class SearchBar extends React.Component {
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                <input placeholder="Where?" onChange={this.handleLocationChange} />
             </div>
             <div className="SearchBar-submit">
-                <a>Let's Go</a>
+                <a onClick={this.handleSearch} >Let's Go</a>
             </div>
         </div>
        )
