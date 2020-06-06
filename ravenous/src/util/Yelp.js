f8d56e99e0e4b74ef2d82bb1c2640242b9f8676f
DanSam%k <dansam5000@gmail.com>
2020-06-06 23:26:27 +0100

    changes

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,14 @@
+const apiKey = '';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { headers: { Authorization: `Bearer ${apiKey}`}}).then(response =>{
+            return response.json();
+        }).then(jsonResponse =>{
+            if(jsonResponse.businesses){
+                
+            }
+        });
+
+    }
+}
